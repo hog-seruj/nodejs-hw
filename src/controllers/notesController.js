ea204b9930d2be5f17b3efd0c3de54b2bc22503e
HOG <hogseruj1@gmail.com>
2026-01-24 20:32:28 +0200

    Updated returned JSON field name.

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -18,17 +18,17 @@ export const getAllNotes = async (req, res) => {
   }
 
   // const notes = await Note.find();
-  const [totalItems, notes] = await Promise.all([
+  const [totalNotes, notes] = await Promise.all([
     noteQuery.clone().countDocuments(),
     noteQuery.skip(skip).limit(perPage),
   ]);
 
-  const totalPages = Math.ceil(totalItems / perPage);
+  const totalPages = Math.ceil(totalNotes / perPage);
 
   res.status(200).json({
     page,
     perPage,
-    totalItems,
+    totalNotes,
     totalPages,
     notes,
   });
